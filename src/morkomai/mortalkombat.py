9bf99080bf8cc63e964c172dcf332b71a9e012a0
Hussein Saafan <hsaafan@uwaterloo.ca>
2022-06-28 20:24:33 -0400

    Added new setting options

--- src/morkomai/mortalkombat.py
+++ src/morkomai/mortalkombat.py
@@ -5,37 +5,49 @@ from .recorder import ScreenRecorder
 
 
 class MortalKombat:
+    """Class used to run Mortal Kombat application.
+
+    Attributes
+    ----------
+    capture_folder: str
+        The folder to store image captures in.
+    save_captures: bool
+        Save the captures with bounding boxes to disk.
+    join_screen: str
+        The path of an image of the screen where players can join.
+    character_select_screen: str
+        The path of an image of the character select screen.
+    continue_screen: str
+        The path of an image of the continue screen.
+    fight_prompt: str
+        Path to an image of the fight prompt.
+    timer_image: str
+        Path to an image of the timer at 00.
+    p_save: float
+        The probability of saving captured images to file.
+    sprite_folder: str
+        The folder that the sprites are stored in.
+    nameplate_folder: str
+        The folder that the name plates are stored in.
+    model_folder: str
+        The folder to store ML models in.
+
+    Parameters
+    ----------
+    dosbox: DOSBox, optional
+        The DOSBox object to run Mortal Kombat on. If none are passed, a
+        new DOSBox object (along with a new Display object) will be
+        created.
+    recorder: ScreenRecorder, optional
+        An object that is used to take snapshots of the screen. If none are
+        passed, a ScreenRecorder object will be created and attached to the
+        dosbox display.
+    settings: dict, optional
+        A dictionary containing all required settings, if none are passed,
+        the settings.yaml file will be loaded.
+    """
     def __init__(self, dosbox: DOSBox = None, recorder: ScreenRecorder = None,
                  settings: dict = None) -> None:
-        """Class used to run Mortal Kombat application.
-
-        Parameters
-        ----------
-        dosbox: DOSBox, optional
-            The DOSBox object to run Mortal Kombat on. If none are passed, a
-            new DOSBox object (along with a new Display object) will be
-            created.
-        recorder: ScreenRecorder, optional
-            An object that is used to take snapshots of the screen. If none are
-            passed, a ScreenRecorder object will be created and attached to the
-            dosbox display.
-        settings: dict, optional
-            A dictionary containing all required settings, if none are passed,
-            the settings.yaml file will be loaded.
-
-        Attributes
-        ----------
-        capture_folder: str
-            The folder to store image captures in.
-        join_screen: str
-            The path of an image of the screen where players can join.
-        character_select_screen: str
-            The path of an image of the character select screen.
-        fight_prompt: str
-            Path to an image of the fight prompt.
-        p_save: float
-            The probability of saving captured images to file.
-        """
         if dosbox is None:
             dosbox = DOSBox()
         self._dosbox = dosbox
@@ -47,7 +59,7 @@ class MortalKombat:
         self._recorder = recorder
 
     def start(self, conf_file: str = 'dos.conf') -> None:
-        """Starts Mortal Kombat.
+        """Starts DOSBox and Mortal Kombat.
 
         Parameters
         ----------
@@ -71,26 +83,44 @@ class MortalKombat:
         settings: dict
             If None, will load settings.yaml file. Takes the following keys:
                 capture_folder: str
-                    The folder to store game screenshots in
+                    The folder to store game screenshots in.
+                save_captures: bool
+                    Save the captures with bounding boxes to disk.
                 join_screen: str
                     Path to an image that signifies that players can start
                     joining.
                 character_screen: str
                     Path to an image that signifies that players can start
                     to select characters.
+                continue_screen: str
+                    The path of an image of the continue screen.
                 fight_prompt: str
                     Path to an image of the fight prompt.
+                timer_image: str
+                    Path to an image of the timer at 00.
                 p_save: float
                     Probability of saving screenshots to file.
+                sprite_folder: str
+                    The folder that the sprites are stored in.
+                nameplate_folder: str
+                    The folder that the name plates are stored in.
+                model_folder: str
+                    The folder to store ML models in.
         """
         if settings is None:
             with open('settings.yaml') as f:
                 settings = yaml.safe_load(f)
         self.capture_folder = abspath(settings['capture_folder'])
+        self.save_captures = settings['save_captures']
         self.join_screen = abspath(settings['join_screen'])
         self.character_screen = abspath(settings['character_screen'])
+        self.continue_screen = abspath(settings['continue_screen'])
         self.fight_prompt = abspath(settings['fight_prompt'])
+        self.timer_image = abspath(settings['timer_image'])
         self.p_save = settings['p(save_image)']
+        self.sprite_folder = abspath(settings['sprite_folder'])
+        self.nameplate_folder = abspath(settings['nameplate_folder'])
+        self.model_folder = abspath(settings['model_folder'])
 
     def stop(self) -> None:
         """Stop Mortal Kombat and the dosbox application."""
